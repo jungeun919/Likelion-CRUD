8c1d397a54d76db9683b36780b31d937076dd327
jungeun919 <renee8369@likelion.org>
2021-05-21 12:57:02 +0900

    update urls.py <str> to <int>

--- crudproject/blog/views.py
+++ crudproject/blog/views.py
@@ -11,17 +11,17 @@ def detail(request, id):
     blog = get_object_or_404(Blog, pk=id)
     return render(request, 'detail.html', {'blog':blog})
 
-def new(request):
+def new(request): # new.html을 띄워주는 함수
     return render(request, 'new.html')
 
-def create(request):
+def create(request): # 입력받은 내용을 데이터베이스에 넣어주는 함수
     new_blog = Blog()
     new_blog.title = request.POST['title']
     new_blog.writer = request.POST['writer']
     new_blog.pub_date = timezone.now()
     new_blog.body = request.POST['body']
     new_blog.save()
-    return redirect('detail', new_blog.id)
+    return redirect('detail', str(new_blog.id))
 
 def edit(request, id):
     edit_blog = Blog.objects.get(id=id)
@@ -34,7 +34,7 @@ def update(request, id):
     update_blog.pub_date = timezone.now()
     update_blog.body = request.POST['body']
     update_blog.save()
-    return redirect('detail', update_blog.id)
+    return redirect('detail', str(update_blog.id))
 
 def delete(request, id):
     delete_blog = Blog.objects.get(id=id)
--- crudproject/crudproject/urls.py
+++ crudproject/crudproject/urls.py
@@ -21,10 +21,10 @@ from blog.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
-    path('<str:id>', detail, name="detail"),
+    path('<int:id>', detail, name="detail"),
     path('new/', new, name="new"),
     path('create/', create, name="create"),
-    path('edit/<str:id>', edit, name="edit"),
-    path('update/<str:id>', update, name="update"),
-    path('delete/<str:id>', delete, name="delete"),
+    path('edit/<int:id>', edit, name="edit"),
+    path('update/<int:id>', update, name="update"),
+    path('delete/<int:id>', delete, name="delete"),
 ]
